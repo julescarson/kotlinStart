e7c741bf285261e544664e87b094340547351980
Jules <metamoniker@gmail.com>
2025-06-26 14:15:28 -0400

    Added day 2 part 2, with debug output

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,11 @@
 fun main() {
 
-    //day 1 part 1
+    //day 1
     println("Day 1 - Part 1: ${day1part1()}")
-
-    //day 1 part 2
     println("Day 1 - Part 2: ${day1part2()}")
 
-    //day 2 part 1
-    println("Day 2 - Part 1: ${day2part1()}")
-
+    //day 2
+    println("Day 2 - Part 1: ${day2part1().first}")
+    println("Day 2 - Part 2: ${day2part2()}")
 
 }
--- src/day2.kt
+++ src/day2.kt
@@ -1,14 +1,17 @@
 import java.io.File
 import kotlin.math.abs
 
-fun day2part1() : Int {
+fun day2part1() : Pair<Int, List<List<Int>>> {
     // safe levels : ASC or DESC on line
     // AND adjacent values within 1 to 3 (incl)
 
     val fileName = "inputText/day2.txt"
-    var safeLevels : Int = 0
+    var safeLevels = 0
+    val badLevels = mutableListOf(listOf<Int>())
 
     File(fileName).forEachLine { line ->
+
+
 
         //int arr from line string
         val curr = line
@@ -22,14 +25,77 @@ fun day2part1() : Int {
             val diff = abs(curr[i] - curr[i+1])
 
             // unsafe conditions
-            if (ascending && curr[i] > curr[i+1]) break
-            else if (!ascending && curr[i] < curr[i+1]) break
-            else if (diff < 1 || diff > 3) break
+            if (ascending && curr[i] > curr[i+1]) {
+                badLevels.add(curr)
+                break
+            }
+            else if (!ascending && curr[i] < curr[i+1]) {
+                badLevels.add(curr)
+                break
+            }
+            else if (diff < 1 || diff > 3) {
+                badLevels.add(curr)
+                break
+            }
 
             //made it to the end of the line (is safe)
             if (i+1 == curr.lastIndex) safeLevels++
         }
     }
-    return safeLevels
+
+    badLevels.removeAt(0) //first list element empty on init
+    return Pair(safeLevels, badLevels)
 }
+
+fun day2part2() : Int {
+
+    // cheese strat: use day 2 part 1, return array of unsafe lines AND safeLevels
+    // use unsafe lines array as test in day2, sum with known safeLevels
+    // really not efficient :D
+
+    val part1 = day2part1()
+    var safeLevels : Int = part1.first
+    val badLevels = part1.second
+
+    val outputDebug = File("d2p2debug.txt")
+    outputDebug.writeText("Day 2 - Part 2 Debug file, check badLevels for each assumed line")
+
+
+    //check each level for allowable cases
+    for (level in badLevels) {
+        outputDebug.appendText("\n$level , ")
+        var badCounter = 0
+        var ascending : Boolean = level[0] < level[1]
+
+
+        for (i in 0 until level.lastIndex) {
+            val difference = abs(level[i] - level[i+1])
+            //changes direction
+            if (ascending && level[i] > level[i+1]) {
+                badCounter++
+                //update direction!
+                ascending = false
+
+            } else if (!ascending && level[i] < level[i+1]) {
+                badCounter++
+                ascending = true
+
+            } else if (difference < 1 || difference > 3) {
+                badCounter++
+
+            }
+
+            if (i+1 == level.lastIndex && badCounter <= 1) {
+                safeLevels++
+            }
+
+        }
+        outputDebug.appendText("badCounter: $badCounter")
+    }
+
+    return safeLevels
+
+}
+
+
 
