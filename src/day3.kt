b09962bf31a0b3b62fc95d1bf8ccf8fc463e1d14
Jules <metamoniker@gmail.com>
2025-06-30 14:16:26 -0400

    Day 3 part 1 complete, REGEX things

--- src/day3.kt
+++ src/day3.kt
@@ -1,5 +1,25 @@
+import java.io.File
+
+
 fun day3part1() : Int {
-    return -1
+    val fileName = "inputText/day3.txt"
+    val file = File(fileName).readText()
+    var sum = 0
+
+
+    // requirements: X and Y are each 1-3 digit
+    // regex pattern already avoids whitespace
+    val pattern = "mul\\(([0-9]{1,3},[0-9]{1,3})\\)"
+        .toRegex()
+
+    pattern.findAll(file)
+        .map { it.groupValues[1] }
+        .forEach { pair ->
+            val nums = pair.split(",")
+            sum += nums[0].toInt() * nums[1].toInt()
+        }
+
+    return sum
 }
 
 
