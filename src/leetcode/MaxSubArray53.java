6504d0b9d44823a325ba2f7174e98b410e64c256
Jules <metamoniker@gmail.com>
2025-11-01 18:39:03 -0400

    Max sub array 53

---
+++ src/leetcode/MaxSubArray53.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class MaxSubArray53 {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 1) return nums[0];
+
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for (int i = 0; i<nums.length; i++) {
+            currSum += nums[i];
+            if (currSum > maxSum) {
+                maxSum = currSum;
+            }
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+}
